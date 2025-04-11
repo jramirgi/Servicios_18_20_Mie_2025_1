220aa70fc4120cd618eafe022b7f1eb4aa9b7b39
jramirgi <joseramirez8046@correo.itm.edu.co>
2025-04-10 21:17:40 -0500

    Add project files.

---
+++ Servicios_18_20/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using Servicios_18_20.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Servicios_18_20
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.MessageHandlers.Add(new TokenValidationHandler());
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Servicios_18_20/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Servicios_18_20.Clases;
+using Servicios_18_20.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Servicios_18_20.Controllers
+{
+    [RoutePrefix("api/Login")]
+    //[Authorize]: Directiva para obligar a que se tenga autorización usar al servicio
+    //[AllowAnonymous]: Directiva para que se pueda usar el servicio sin autorización.
+    [AllowAnonymous]
+    public class LoginController : ApiController
+    {
+        [HttpPost]
+        [Route("Ingresar")]
+        public IQueryable<LoginRespuesta> Ingresar(Login login)
+        {
+            clsLogin _Login = new clsLogin();
+            _Login.login = login;
+            return _Login.Ingresar();
+        }
+    }
+}
