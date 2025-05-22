a5a332c1d1b418445ef7aa264030d4dc7d92525c
jramirgi <joseramirez8046@correo.itm.edu.co>
2025-05-21 19:34:17 -0500

    Clientes - Telefonos

---
+++ Servicios_18_20/Clases/clsTipoTelefono.cs
@@ -0,0 +1,23 @@
+﻿using Servicios_18_20.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servicios_18_20.Clases
+{
+    public class clsTipoTelefono
+    {
+        private DBSuperEntities dbSuper = new DBSuperEntities();
+        public IQueryable LlenarCombo()
+        {
+            return from T in dbSuper.Set<TIpoTElefono>()
+                   orderby T.Nombre
+                   select new
+                   {
+                       Codigo = T.Codigo,
+                       Nombre = T.Nombre
+                   };
+        }
+    }
+}
